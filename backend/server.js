2611453a130b62d3fbc029cd5c81010d36274dd0
Hardik Rana <rana.hardik1997.hr.gmail.com>
2024-08-10 23:42:48 -0400

    Update build scripts

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
+
 const connectDB = require('./config/db');
 
 const userRoutes = require('./routes/userRoutes');
